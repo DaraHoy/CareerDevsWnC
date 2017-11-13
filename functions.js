ac6abf9d7d4aa8dfdb5bf173711d6a37c9279e81
Dara Hoy <darahoy@gmail.com>
2017-11-13 15:33:34 +0000

    test commit

---
+++ functions.js
@@ -0,0 +1,13 @@
+// makeTurkeySandwich
+//     Get one slice of bread.
+//     Add turkey.
+//     Put a slice of bread on top.
+
+function makeTurkeySandwich() {
+    Get one slice of bread;
+    Add turkey;
+    Put a slice of bread on top;
+}
+
+// To run function add () at the end of function
+makeTurkeySandwich()
