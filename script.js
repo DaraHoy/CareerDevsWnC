1e85675bb5d9f4a69e6e0a636ed3492a0d0a964e
Dara Hoy <darahoy@gmail.com>
2017-12-28 20:55:49 +0000

    Complete ver 4 requirements

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 var todoList = {
-    todos: ['item 1', 'item 2', 'item 3'],
+    todos: [],
     displayTodos: function() {
         console.log('My Todos:', this.todos);
     },
-    addTodo: function(todo) {
-        this.todos.push(todo);
+    addTodo: function(todoText) {
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        });
         this.displayTodos();
     },
-    changeTodo: function(position, value) {
-        this.todos[position] = value;
+    changeTodo: function(position, todoText) {
+        this.todos[position].todoText = todoText;
         this.displayTodos();
     },
     deleteTodo: function(position) {
         this.todos.splice(position, 1);
         this.displayTodos();
+    },
+    toggleCompleted: function(position) {
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();
     }
 };
+
+console.log(todoList.addTodo('this is an object'))
+console.log(todoList.changeTodo(0, 'changed'))
+console.log(todoList.toggleCompleted(0))
