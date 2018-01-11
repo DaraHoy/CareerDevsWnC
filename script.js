cf6dddd98c572a4390f3561570de4d22e0176928
Dara Hoy <darahoy@gmail.com>
2018-01-11 15:20:38 +0000

    Complete ver 11 requirements

--- script.js
+++ script.js
@@ -19,21 +19,25 @@ var todoList = {
     toggleAll: function() {
         var totalTodos = this.todos.length;
         var completedTodos = 0;
-        for (var i = 0; i < totalTodos; i++) {
-            if (this.todos[i].completed === true) {
+        //Get number of todos completed.
+        this.todos.forEach(function(todo) {
+            if (todo.completed === true) {
                 completedTodos++;
             }
-        }
-        if (completedTodos === totalTodos) {
-            for (var i = 0; i < totalTodos; i++) {
-                this.todos[i].completed = false;
+        });
+
+
+
+        this.todos.forEach(function(todo) {
+            //case 1: if everything is true, make everything false
+            if (completedTodos === totalTodos) {
+                todo.completed = false;
             }
-        }
-        else {
-            for (var i = 0; i < totalTodos; i++) {
-                this.todos[i].completed = true;
+            //case 2: Otherwise, make everything true
+            else {
+                todo.completed = true
             }
-        }
+        })
     }
 };
 
@@ -72,9 +76,8 @@ var view = {
     displayTodos: function() {
         var todoUl = document.querySelector('ul');
         todoUl.innerHTML = "";
-        for (var i = 0; i < todoList.todos.length; i++) {
+        todoList.todos.forEach(function(todo, position) {
             var todoLi = document.createElement('li');
-            var todo = todoList.todos[i];
             var todoTextWithCompletion = '';
 
             if (todo.completed === true) {
@@ -84,11 +87,11 @@ var view = {
                 todoTextWithCompletion = '( ) ' + todo.todoText;
             }
 
-            todoLi.id = i;
+            todoLi.id = position;
             todoLi.textContent = todoTextWithCompletion;
             todoLi.appendChild(this.createDeleteButton());
             todoUl.appendChild(todoLi);
-        }
+        }, this)
     },
     createDeleteButton: function() {
         var deleteButton = document.createElement('button');
